ddad507adaee74b206d4a1810c3170f0fafcc78c
Roman Berli <roman.berli2007@gmail.com>
2023-11-22 16:12:19 +0100

    Finished Watch, responsive and designed well

--- analogClock.js
+++ analogClock.js
@@ -4,8 +4,15 @@ const hr = document.querySelector(".hand.hour-hand");
 const min = document.querySelector(".hand.min-hand");
 const sec = document.querySelector(".hand.second-hand");
 
+let oldDate = 0;
+let oldMonth = 0;
+let oldYear = 0;
 // Setting up the loop wich changes to rotation of the clock hands when required
+//Settinh up a transformazion affect
 
+hr.style.transition = "transform 0.5s linear";
+min.style.transition = "transform 0.5s linear";
+sec.style.transition = "transform 0.5s linear";
 setInterval(() => {
   // Extracting the current time from DATE() function
 
@@ -13,7 +20,10 @@ setInterval(() => {
   let hour = day.getHours();
   let minutes = day.getMinutes();
   let seconds = day.getSeconds();
-
+  //For Date
+  let date = day.getDate();
+  let month = day.getMonth() + 1;
+  let year = day.getFullYear();
   //Formula for the rotation of hour, minute and second
 
   let hrrotation = 30 * hour + 0.5 * minutes;
@@ -25,4 +35,36 @@ setInterval(() => {
   hr.style.transform = `translate(-50%,-100%) rotate(${hrrotation}deg)`;
   min.style.transform = `translate(-50%,-100%) rotate(${minrotation}deg)`;
   sec.style.transform = `translate(-50%,-85%) rotate(${secrotation}deg)`;
+
+  //Writing Date
+  if (date != oldDate) {
+    document.querySelector(".hand.second-hand").after(
+      Object.assign(document.createElement("p"), {
+        textContent: date,
+        id: "date",
+      })
+    );
+
+    document.querySelector(".hand.second-hand").after(
+      Object.assign(document.createElement("p"), {
+        textContent: date + "." + month + "." + year,
+        id: "datePopUp",
+      })
+    );
+    oldDate = date;
+    oldMonth = month;
+    oldYear = year;
+  }
+  let datePopUper = document.querySelector("#datePopUp");
+  document.addEventListener("mouseover", function (event) {
+    if (event.target.id === "date") {
+      datePopUper.style.display = "block";
+    }
+  });
+
+  document.addEventListener("mouseout", function (event) {
+    if (event.target.id === "date") {
+      datePopUper.style.display = "none";
+    }
+  });
 });
