6d72ed6036aeec23eb576ff67209aad7a9157df5
Roman Berli <roman.berli2007@gmail.com>
2023-11-22 11:31:52 +0100

    Code is commented and github-repo file added.

--- analogClock.js
+++ analogClock.js
@@ -1,23 +1,28 @@
-const hr = document.querySelector('.hand.hour-hand') 
-const min = document.querySelector('.hand.min-hand') 
-const sec = document.querySelector('.hand.second-hand') 
+// Selecting all of the css classes which i want to manipulate
 
- 
-setInterval(() => { 
-	let day = new Date() 
-	let hour = day.getHours() 
-	let minutes = day.getMinutes() 
-	let seconds = day.getSeconds() 
+const hr = document.querySelector(".hand.hour-hand");
+const min = document.querySelector(".hand.min-hand");
+const sec = document.querySelector(".hand.second-hand");
 
-	 
-	let hrrotation = (30 * hour) + (0.5 * minutes); 
-	let minrotation = 6 * minutes; 
-	let secrotation = 6 * seconds; 
+// Setting up the loop wich changes to rotation of the clock hands when required
 
-	hr.style.transform = 
-		`translate(-50%,-100%) rotate(${hrrotation}deg)` 
-	min.style.transform = 
-		`translate(-50%,-100%) rotate(${minrotation}deg)` 
-	sec.style.transform = 
-		`translate(-50%,-85%) rotate(${secrotation}deg)` 
+setInterval(() => {
+  // Extracting the current time from DATE() function
+
+  let day = new Date();
+  let hour = day.getHours();
+  let minutes = day.getMinutes();
+  let seconds = day.getSeconds();
+
+  //Formula for the rotation of hour, minute and second
+
+  let hrrotation = 30 * hour + 0.5 * minutes;
+  let minrotation = 6 * minutes;
+  let secrotation = 6 * seconds;
+
+  //manipulating the css. (We set a rotation for the clock hands)
+
+  hr.style.transform = `translate(-50%,-100%) rotate(${hrrotation}deg)`;
+  min.style.transform = `translate(-50%,-100%) rotate(${minrotation}deg)`;
+  sec.style.transform = `translate(-50%,-85%) rotate(${secrotation}deg)`;
 });
